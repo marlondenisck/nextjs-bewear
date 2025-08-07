4d4bf5977e06084ebc65c9bd6cdb26a03b998276
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-08-07 10:54:13 -0400

    adicionado eslint e shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <h1>ola mundo</h1>
-  );
+    <>
+      <h1>ola mundo</h1>
+      <Button>
+        Click me
+      </Button>
+    </>
+  )
 }
