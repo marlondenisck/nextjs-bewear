f9446a4bf4f63d590ac83c394440b15a012dd5b8
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-09-01 21:57:26 -0400

    listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 import { desc } from "drizzle-orm";
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
-// import CategorySelector from "@/components/common/category-selector";
-// import Footer from "@/components/common/footer";
-// import { Header } from "@/components/common/header";
-// import ProductList from "@/components/common/product-list";
+import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
 import { productTable } from "@/db/schema";
 
 const Home = async () => {
-  // const products = await db.query.productTable.findMany({
-  //   with: {
-  //     variants: true,
-  //   },
-  // });
-  // const newlyCreatedProducts = await db.query.productTable.findMany({
-  //   orderBy: [desc(productTable.createdAt)],
-  //   with: {
-  //     variants: true,
-  //   },
-  // });
-  // const categories = await db.query.categoryTable.findMany({});
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -38,10 +38,10 @@ const Home = async () => {
           />
         </div>
 
-        {/* <ProductList products={products} title="Mais vendidos" /> */}
+        <ProductList products={products} title="Mais vendidos" />
 
         <div className="px-5">
-          {/* <CategorySelector categories={categories} /> */}
+          <CategorySelector categories={categories} />
         </div>
 
         <div className="px-5">
@@ -55,8 +55,8 @@ const Home = async () => {
           />
         </div>
 
-        {/* <ProductList products={newlyCreatedProducts} title="Novos produtos" /> */}
-        {/* <Footer /> */}
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/common/category-selector.tsx
@@ -0,0 +1,27 @@
+import { categoryTable } from "@/db/schema";
+
+import { Button } from "../ui/button";
+
+interface CategorySelectorProps {
+  categories: (typeof categoryTable.$inferSelect)[];
+}
+
+const CategorySelector = ({ categories }: CategorySelectorProps) => {
+  return (
+    <div className="rounded-3xl bg-[#F4EFFF] p-6 sm:p-4 xs:p-1">
+      <div className="grid grid-cols-2 gap-3">
+        {categories.map((category) => (
+          <Button
+            key={category.id}
+            variant="ghost"
+            className="rounded-full bg-white text-xs font-semibold truncate"
+          >
+            {category.name}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategorySelector;
