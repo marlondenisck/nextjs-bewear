f040d67b84c68e5bbf1b9da00ecc6973908ccdc6
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-09-04 23:15:20 -0400

    adicionado suporte ao carrinho de compras com funcionalidades de adicionar, obter e exibir itens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
 import { Toaster } from "@/components/ui/sonner";
+import ReactQueryProvider from "@/providers/react-query";
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,8 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-         <Toaster position="top-center" richColors />
+        <ReactQueryProvider>
+          {children}
+          <Toaster position="top-center" richColors />
+        </ReactQueryProvider>
       </body>
     </html>
   )
--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -9,13 +9,16 @@ interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
-   textContainerClassName?: string;
+  textContainerClassName?: string;
 }
 
 const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
   return (
-    <Link href="/" className="flex flex-col gap-4">
+    <Link
+      href={`/product-variant/${firstVariant.slug}`}
+      className="flex flex-col gap-4"
+    >
       <Image
         src={firstVariant.imageUrl}
         alt={firstVariant.name}
